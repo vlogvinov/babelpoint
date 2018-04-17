726ef11dd3e83765bbefaf8c7810eeb1058f37f5
Vladimir Logvinov <vladimir.logvinov@valtech.com>
2018-04-17 22:18:42 +0300

    ScenarioHooks implementation

--- Babelpoint/PageObjectModel/Utils/Hooks/ScenariousHooks.cs
+++ Babelpoint/PageObjectModel/Utils/Hooks/ScenariousHooks.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using PageObjectModel.Utils.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechTalk.SpecFlow;
 
 namespace PageObjectModel.Utils.Hooks
 {
-    class ScenariousHooks
+    internal static class ScenariousHooks
     {
+        [BeforeScenario]
+        internal static void StartWebDriver()
+        {
+            if (ScenarioContext.Current.ScenarioInfo.Tags.Contains("PhantomJs"))
+            {
+                DriverController.Instance.StartPhantomJs();
+            }
+            else if (ScenarioContext.Current.ScenarioInfo.Tags.Contains("Chrome"))
+            {
+                DriverController.Instance.StartChrome();
+            }
+            else if (ScenarioContext.Current.ScenarioInfo.Tags.Contains("Debug"))
+            {
+                DriverController.Instance.StartChrome();
+            }
+            else
+            {
+                DriverController.Instance.StartChrome();
+            }
+        }
+
+        [AfterScenario]
+        internal static void StopWebDriver()
+        {
+            if (!ScenarioContext.Current.ScenarioInfo.Tags.Contains("Debug"))
+            {
+                DriverController.Instance.StopWebDriver();
+            }
+        }
     }
 }
