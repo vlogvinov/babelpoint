5082095864eddc9391a1959b7669b48de99064bc
Vladimir Logvinov <vladimir.logvinov@valtech.com>
2018-04-17 22:25:32 +0300

    BasePage implementation

--- Babelpoint/PageObjectModel/Pages/BasePage.cs
+++ Babelpoint/PageObjectModel/Pages/BasePage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace PageObjectModel.Pages
 {
-    class BasePage
+    public class BasePage : Page
     {
+        public IWebDriver Driver { get; internal set; }
+
     }
 }
