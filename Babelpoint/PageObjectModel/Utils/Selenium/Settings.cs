b19d9b7ad5f9a10b836eaf6250bd58cf68bcf0b6
Vladimir Logvinov <vladimir.logvinov@valtech.com>
2018-04-18 20:34:16 +0300

    added configuration into app.config

--- Babelpoint/PageObjectModel/Utils/Selenium/Settings.cs
+++ Babelpoint/PageObjectModel/Utils/Selenium/Settings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace PageObjectModel.Utils.Selenium
 {
     public class Settings
     {
-        public static string BaseUrl = "https://www.wikipedia.org/";
-        public static string WelcomeMessage = ":: Welcome to the Wikipedia";
+        public static string BaseUrl = ConfigurationManager.AppSettings["BaseUrl"];
+        public static string WelcomeMessage = ConfigurationManager.AppSettings["WelcomeMessage"];
     }
 }
